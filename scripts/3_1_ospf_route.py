e6e4433e9e0421ff6b3dfac0a0d26e57e202d055
highlightnn <58547018+highlightnn@users.noreply.github.com>
2020-01-19 08:59:05 +0300

    Update 3_1_ospf_route.py

--- scripts/3_1_ospf_route.py
+++ scripts/3_1_ospf_route.py
@@ -1,2 +1,17 @@
 ospf_route = "O                      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
-Protocol, Prefix, AD/Metric, _, Next-Hop, Last update, Outbound Interface = ospf_route.split()
+route = ospf_route.split()
+a, a1 = ['Protocol:', route[0].replace('O', 'OSPF')]
+b, b1 = ['Prefix:',route[1]]
+c, c1 = ['AD/Metric:',route[2].strip('[]')]
+d, d1 = ['Next-Hop:',route[4].strip(',')]
+e, e1 = ['Last update:',route[5].strip(',')]
+f, f1 = ['Outbound Interface:',route[6]]
+result_table = '''
+    {:24} {:24}
+    {:24} {:24}
+    {:24} {:24}
+    {:24} {:24}
+    {:24} {:24}
+    {:24} {:24}
+    '''
+print(result_table.format(a, a1, b, b1, c, c1, d, d1, e, e1, f, f1))
